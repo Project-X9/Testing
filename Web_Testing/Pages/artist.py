42ec10e54c608914353153bb8791a9fbe46e082e
Eman Gamal <eman_jamal@yahoo.com>
2020-04-11 23:44:09 +0200

    Add artist

--- Web_Testing/Pages/artist.py
+++ Web_Testing/Pages/artist.py
@@ -17,18 +17,14 @@ class Artist(WebPlayerMenu):
           A string containing the xpath of log out button
     account_btn : string
           A string containing the link text of account button
-    upgrade_btn : string
-          A string containing the link text of upgrade button
-    playlist_cards : string
-          A string containing the class name of playlist cards
-    no_of_songs_xpath : string
-          A string containing the xpath of number of song in the playlist
-    playlist_name_class_name : string
-          A string containing the class name of playlist's name in the playlist page
-    songs_container : string
-          A string containing the class name of songs in the playlist
-    no_of_songs : integer
-          An integer containing the number of songs displayed in the playlist page
+    follow_btn : string
+          A string containing follow button
+    artist_name : string
+          A string containing the class name of artist name in the artist page
+    artist_name_about : string
+          A string containing the class name of artist name in the about page
+    about : string
+          A string containing the xpath of about button
     playlist_name_card : string
          A sting containing the name of the play list from the playlist card
     page_left_btn : WebDriverElement
@@ -36,24 +32,12 @@ class Artist(WebPlayerMenu):
     page_right_btn : WebDriverElement
          a Web driver element representing the Page right button
 
-    Methods
-    -------
-    click_liked_songs()
-        Clicks the Liked Songs Play button in the card
-    click_liked_songs_txt()
-        Clicks the Liked Songs Text button in the card
-    check_liked_songs_click()
-        Checks if clicking the Liked Songs card goes to the right page
-    check_card_click(card_no, report_allure)
-        Checks if clicking a playlist card goes to the right page
-
     """
 
     profile_btn = "//*[@id='root']/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div/div/nav/ul[2]/li[2]/li/div[1]/button"
     logout_btn = "//*[@id='root']/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div/div/nav/ul[2]/li[2]/li/div[2]/button[3]/button"
     account_btn = "Account"
     follow_btn = "//*[@id='followButton']"
-    related_artist = "//*[@id='ab']"
     about = "/html/body/div[1]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[3]/button[3]"
     page_left_btn = "/html/body/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/nav/div/div/div/ul/li[1]/button/a"
     page_right_btn = "/html/body/div/div/div/div/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/nav/div/div/div/ul/li[2]/button/a"
@@ -90,11 +74,6 @@ class Artist(WebPlayerMenu):
         self.click_button_safe(self.find_element_by_xpath(self.logout_btn))
 
     def click_related_artist(self):
-        """Clicks Profile button"""
-        self.click_button_safe(self.find_element_by_xpath(self.related_artist))
-
-    def click_related_artist(self):
-        """Clicks Profile button"""
         self.click_button_safe(self.find_element_by_xpath(self.related_artist))
 
     def check_follow(self):
@@ -116,7 +95,7 @@ class Artist(WebPlayerMenu):
             return False
 
     def click_follow(self):
-        """Clicks upgrade button"""
+        """Clicks follow button"""
         self.click_button_safe(self.find_element_by_xpath(self.follow_btn))
 
     def click_webplayer(self):
