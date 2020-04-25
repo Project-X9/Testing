1c1ac2df34a39c2741d4a53ca1383343f8c1332d
Mohammad Osama <mohdos_1999@hotmail.com>
2020-04-25 02:35:37 +0200

    - Updated requirements.txt

--- Web_Testing/helperClasses.py
+++ Web_Testing/helperClasses.py
@@ -120,7 +120,7 @@ class WebHelper:
 
     """
 
-    base_url = "http://localhost/"
+    base_url = "http://localhost:4000/"
     month_dict = {"January": 1, "February": 2, "March": 3
         , "April": 4, "May": 5, "June": 6
         , "July": 7, "August": 8, "September": 9
